497f5f71b85f76fd17b6dad8833859cb67f21027
Filip Kin <filip@kinmails.com>
2020-09-21 11:14:57 -0400

    aaaa

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ basically this is a solidworks macro with a couple event listens for active doc 
 then it collects the magic and sends it to a JS script
 which guesses at what the project name is and sends it to the wakatime script
 drop this project into `%appdata%\solidworks-wakatime` or change the paths yourself
+download [wakatime-cli](https://github.com/wakatime/wakatime/releases) to this folder
 I recommend setting up wakatime with another editor like VS Code first because this plugin doesn't have any ability to setup wakatime or even sense if it's there or not
 Still trying to figure out how to get the macro to load automatically
--- index.js
+++ index.js
@@ -2,6 +2,23 @@ let args = process.argv;
 let folders = {};
 let project, previous;
 let asmCount = 0;
+let fs = require('fs');
+
+var logger = fs.createWriteStream(process.env['APPDATA']+'\\solidworks-wakatime\\log.txt', {
+  flags: 'a' // 'a' means appending (old data will be preserved)
+})
+
+let pad = (n) => {
+  return n.toString().padStart(2, 0)
+}
+
+let log = (msg) => {
+  let now = new Date();
+  let timestamp = `[${now.getFullYear()}-${pad(now.getMonth())}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}] `;
+  logger.write(timestamp + msg + '\n');
+}
+
+log('Sending heartbeat');
 
 for (let doc of args) {
 	if (doc.toUpperCase().endsWith('ASM')) {
@@ -25,6 +42,14 @@ for (let [folder, count] of Object.entries(folders)) {
 
 if (project == undefined) project = previous;
 
-let cmd = 'wakatime-cli.exe --verbose --entity "'+args[2]+'" --project "'+project+'" --language "CAD" --plugin "solidworks-wakatime/0.0.1"'+((args[2] == true)?' --write':'');
-require('child_process').exec(cmd);
-require('fs').writeFileSync('%appdata%\\solidworks-wakatime\\solidworks-wakatime.swblog.txt', new Date().getTime() + cmd);
+log('Project: '+project);
+
+let cmd = '"'+process.env['APPDATA']+'\\solidworks-wakatime\\wakatime\\wakatime-cli.exe" --verbose --entity "'+args[2]+'" --project "'+project+'" --language "CAD" --plugin "solidworks-wakatime/0.0.1"'+((args[2] == true)?' --write':'');
+log(cmd);
+try {
+  require('child_process').exec(cmd);
+} catch(err) {
+  log('failed '+err);
+}
+log('Complete');
+logger.end();
